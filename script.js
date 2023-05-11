492986e633ed9e9e61925b7aad6fb77e1a1b1c34
Chaitanya <77605781+cha1tany4@users.noreply.github.com>
2023-05-11 08:20:04 +0530

    yes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const horizontalSections = gsap.utils.toArray('section.horizontal')
 
-horizontalSections.forEach(function (sec, i) {	
+horizontalSections.forEach(function (sec, i) {  
 
   var thisPinWrap = sec.querySelector('.pin-wrap');
   var thisAnimWrap = thisPinWrap.querySelector('.animation-wrap');
@@ -13,7 +13,7 @@ horizontalSections.forEach(function (sec, i) {
     x: () => thisAnimWrap.classList.contains('to-right') ? getToValue() : 0, 
     ease: "none",
     scrollTrigger: {
-      trigger: sec,		
+      trigger: sec,   
       start: "top top",
       end: () => "+=" + (thisAnimWrap.scrollWidth - window.innerWidth),
       pin: thisPinWrap,
@@ -28,20 +28,27 @@ horizontalSections.forEach(function (sec, i) {
 
 // p5js
 
-let sketch = p => {
-  var img;
-  p.preload = function() {
-    img = loadImage('map-form.png');
-  }
-  p.setup = function(){
-    p.createCanvas(710, 800);
-    p.background(255);
-  }
-  p.draw = function() {
+function preload() {
+
+}
+
+function setup() {
+  var canvas = createCanvas(568, 640);
+  canvas.parent('canvas');
+  background(127);
+  loadImage('./map-form.png', img => {
+    img.resize(568, 640);
     image(img, 0, 0);
+  }, console.log("error")); 
+}
+
+function draw() {
+  stroke(255,2,16);
+  strokeWeight(10);
+  if (mouseIsPressed === true) {
+    line(mouseX, mouseY, pmouseX, pmouseY);
   }
-};
-new p5(sketch, 'canvas');
+}
 
 
 // MODAL
@@ -52,15 +59,21 @@ var modal = document.getElementById("myModal");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("img1");
 var modalImg = document.getElementById("img01");
-img.onclick = function(){
+function wallimageclick(clicked_id) {
   modal.style.display = "block";
-  modalImg.src = this.src;
+  modalImg.src = document.getElementById(clicked_id).src;
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("myClose")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
   modal.style.display = "none";
 }
+
+
+// AOS
+
+AOS.init();
+
